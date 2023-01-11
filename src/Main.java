6329aef75319361f639ca45d51a656451e02767c
luc-nguyen'174 <louiskingvn@gmail.com>
2023-01-11 15:24:26 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //nhap thong tin san pham
+        String[][] production = new String[5][5];
+        for (int i = 0; i < production.length;i++) {
+            String id = String.valueOf(i+1);
+            production[i][0]= id;
+            System.out.println("Nhap thong tin san pham " + id);
+
+            System.out.print("Nhap vao ten san pham: ");
+            String name = scanner.nextLine();
+            production[i][1]= name;
+
+            System.out.print("Nhap vao gia: ");
+            String price = scanner.nextLine();
+            production[i][2]= price;
+
+            System.out.print("Nhap vao so luong: ");
+            String quantity = scanner.nextLine();
+            production[i][3]= quantity;
+
+            System.out.print("Nhap vao discount: ");
+            String discount = scanner.nextLine();
+            production[i][4]= discount;
+        }
+        Product product = new Product();
+
+    }
+}
